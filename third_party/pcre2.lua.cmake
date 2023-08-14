91d82cfc5fbbef1184dbd00888ec3fdaaca78d61
qgymib <qgymib@gmail.com>
2023-08-14 08:45:43 +0800

    rename third_party/pcre2.lua

--- third_party/pcre2.lua.cmake
+++ third_party/pcre2.lua.cmake
@@ -1,18 +1,18 @@
-set(LUA_PCRE2_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/third_party/lua_pcre2)
+set(LUA_PCRE2_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/third_party/pcre2.lua)
 
-add_library(lpcre2
+add_library(pcre2.lua
     ${LUA_PCRE2_ROOT}/src/pcre2.lua.c)
 
-target_include_directories(lpcre2
+target_include_directories(pcre2.lua
     PUBLIC
         $<INSTALL_INTERFACE:include>
         $<BUILD_INTERFACE:${LUA_PCRE2_ROOT}/include>
     PRIVATE
         ${LUA_PCRE2_ROOT}/src)
 
-target_link_libraries(lpcre2
+target_link_libraries(pcre2.lua
     PRIVATE
         lua54
         pcre2)
 
-target_compile_options(lpcre2 PRIVATE -DPCRE2_STATIC)
+target_compile_options(pcre2.lua PRIVATE -DPCRE2_STATIC)
